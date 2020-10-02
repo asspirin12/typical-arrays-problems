2b6151099f0f1179bd61944f7048a41587fea2d0
Siarhei Kazlouski <aspirin12@yandex.ru>
2020-10-02 22:06:12 +0300

    feat: add functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        let minItem = array[0];
+        for (let i = 1; i < array.length; i++) {
+            if (array[i] < minItem) {
+                minItem = array[i];
+            } else {
+                continue;
+            }
+        }
+        return minItem;
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        let maxItem = array[0];
+        for (let i = 1; i < array.length; i++) {
+            if (array[i] > maxItem) {
+                maxItem = array[i];
+            } else {
+                continue;
+            }
+        }
+        return maxItem;
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        let sum = 0;
+        for (let i = 0; i < array.length; i++) {
+            sum = sum + +array[i];
+        }
+        return sum / array.length;
+    }
 }
+
